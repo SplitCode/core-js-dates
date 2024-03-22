47c2790e114e70bad1a1a501051c25e50463675d
Natalia Bandurova <natacsp@mail.ru>
2024-03-23 01:41:28 +0700

    feat: add solution for getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -51,7 +51,18 @@ function getTime(date) {
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
 function getDayName(/* date */) {
-  throw new Error('Not implemented');
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const day = new Date(date).getUTCDay();
+  return weekDays[day];
 }
 
 /**
@@ -65,8 +76,10 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  // const currentDay = date.getDay();
+
+  // return new Date(date).toJSON();
 }
 
 /**
